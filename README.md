a19d1c1e42eccdc7c572bc751b66f64f7b63bd28
jascha <jascha@jaeh.at>
2020-04-03 07:21:58 +0200

    add meta files

---
+++ README.md
@@ -0,0 +1,5 @@
+## slides.webboot.org
+
+these are slides for my talk about https://webboot.org
+
+talk held via interwebs at https://noncon.org on 03. April 2020, 14:40 viennese time
---
+++ config.mjs
@@ -0,0 +1,9 @@
+export default {
+  ROOT: 'src',
+  WEB_ROOT: '/',
+  URL: 'slides.webboot.org',
+  CNAME: 'slides.webboot.org',
+  PUBLIC: 'docs',
+  THEME: 'example',
+  HOIST: ['LightSwitch', 'Gdpr'],
+}
